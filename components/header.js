740afc02249708b8d7d40b74479191fd1b13c7f7
Jeff Tegeler <jefftegeler@gmail.com>
2022-07-04 12:19:01 -0400

    changes

--- components/header.js
+++ components/header.js
@@ -3,14 +3,16 @@ import Image from 'next/image'
 import Search from '../assets/svg/search'
 import { ConnectButton } from 'web3uikit'
 
-const styles = { header: 'bg-[#991b1b] text-white h-20 flex gap-[100px] w-full p-[10px]',
-                  headerwrapper: 'flex justify-center h-full max-w-screen-xl mx-auto px-4',
-                  nav: 'flex justify-center items-center gap-[20px]',
-                  navItem: 'relative mr-1 cursor-pointer hover:opacity-60',
-                  badge: 'rounded-full bg-sky-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-2 ring-sky-600',
-                  inputContainer: 'flex items-center justify-center p-2 rounded bg-[#7f1d1d]',
-                  input: 'bg-transparent outline-none text-white w-70 mL-3'
-                }
+const styles = 
+{ header: 'bg-[#991b1b] text-white h-20 flex gap-[100px] w-full p-[10px]',
+  headerwrapper: 'flex justify-center h-full max-w-screen-xl mx-auto px-4',
+  nav: 'flex justify-center items-center gap-[20px]',
+  navItem: 'relative mr-1 cursor-pointer hover:opacity-60',
+  navLink: 'text-white flex mx-[10px]',
+  badge: 'rounded-full bg-sky-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-2 ring-sky-600',
+  inputContainer: 'flex items-center justify-center p-2 rounded bg-[#7f1d1d]',
+  input: 'bg-transparent outline-none text-white w-70 mL-3'
+  }
 
 const header = () => {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next'
 import  Header from '../components/header'
+import Trending from '../components/trending'
+import CMCTable from '../components/cmc-table/CmcTable'
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'> 
       <Header />
       <div className='mt-10'/>
-      {/* <trending /> */}
+      <Trending />
       <div className='mt-20'/>
-      {/* <bmctable /> */}
+      <CMCTable/>
     </div>
   )
 }
